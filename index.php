1816a83feeba17a67e4666f7b6db4833aa09dda9
Abed Putra <abedputra@gmail.com>
2016-07-22 19:50:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Ip Location</title>
+</head>
+
+<body>
+<?php
+// Preset PHP settings
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+set_time_limit(0);
+
+require_once('ip2location.class.php');
+
+$loc = new IP2Location('IP-COUNTRY-SAMPLE.BIN', IP2Location::FILE_IO);
+
+
+$ip = ''.$_SERVER['REMOTE_ADDR'].'';
+?>
+Your City : <?php echo $loc->lookup($ip, IP2Location::CITY_NAME); ?>
+<br>
+Your Region : <?php echo $loc->lookup($ip, IP2Location::REGION_NAME); ?>
+<br>
+Your Country : <?php echo $loc->lookup($ip, IP2Location::COUNTRY_NAME); ?>
+<body>
+</html>
