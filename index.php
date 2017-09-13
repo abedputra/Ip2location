190aa47ce1ee62c1a319ec638037c36a4548c378
Abed Putra <abedputra@gmail.com>
2017-09-13 14:09:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 set_time_limit(0);
 
-require_once('ip2location.class.php');
-
+require_once('ip2location.class.php'); //add class
 $loc = new IP2Location('IP-COUNTRY-SAMPLE.BIN', IP2Location::FILE_IO);
-
-
-$ip = ''.$_SERVER['REMOTE_ADDR'].'';
+$ip = ''.$_SERVER['REMOTE_ADDR'].''; //get ip user
 ?>
 Your City : <?php echo $loc->lookup($ip, IP2Location::CITY_NAME); ?>
 <br>
